38579bf701b7e9ec9d93d49d461340ccef4b4348
Jeff <Jeff@Jeff-PC>
2012-10-05 22:30:20 +0200

    Empty project

---
+++ SpaceSim/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Jeff
+ *
+ * Created on October 5, 2012, 10:29 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello World!\nJust testing this shit.";
+    return 0;
+}
+
